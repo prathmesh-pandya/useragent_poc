d68ac9486e436b33a8e0fd9f5734223e884f78a8
Prathmesh Pandya <prathmesh.pandya@krishtechnolabs.com>
2024-04-05 14:20:05 +0530

    fourth

--- src/app/slider/Child.jsx
+++ src/app/slider/Child.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 
 const Child = ({ isMobileView, data }) => {
   alert(`${data}=  this is data , ${isMobileView}= this is mobile view  `);
-  return <div>Child</div>;
+  return (
+    <div>{isMobileView ? 'Hello from mobile ' : 'Hello from desktop '}</div>
+  );
 };
 
 export default Child;
