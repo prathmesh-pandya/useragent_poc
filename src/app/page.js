16735e1567f83f42ea29248a06c9d4d234b7380f
Prathmesh Pandya <prathmesh.pandya@krishtechnolabs.com>
2024-04-05 14:26:38 +0530

    fifth

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
   
   let isMobileView = isMobileUserAgent(headerList.get('user-agent'));
   console.log(isMobileView, 'isMobileView this is mibile view for server  ');
-  return <Child isMobileView={isMobileView} data={dat} />;
+  return <Child isMobileView={isMobileView} />;
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
--- src/app/slider/Child.jsx
+++ src/app/slider/Child.jsx
@@ -1,8 +1,8 @@
 'use client';
 import React from 'react';
 
-const Child = ({ isMobileView, data }) => {
-  alert(`${data}=  this is data , ${isMobileView}= this is mobile view  `);
+const Child = ({ isMobileView }) => {
+  //   alert(`${data}=  this is data , ${isMobileView}= this is mobile view  `);
   return (
     <div>{isMobileView ? 'Hello from mobile ' : 'Hello from desktop '}</div>
   );
